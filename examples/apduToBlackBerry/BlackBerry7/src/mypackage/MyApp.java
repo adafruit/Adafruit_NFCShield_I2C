39142eee02578b9664dfb5353a10c178b4d65142
Rob Williams <robwilliams@rim.com>
2013-07-09 11:02:09 -0400

    Version of code used in demo video.

---
+++ examples/apduToBlackBerry/BlackBerry7/src/mypackage/MyApp.java
@@ -0,0 +1,32 @@
+package mypackage;
+
+import net.rim.device.api.ui.UiApplication;
+
+/**
+ * This class extends the UiApplication class, providing a
+ * graphical user interface.
+ */
+public class MyApp extends UiApplication
+{
+    /**
+     * Entry point for application
+     * @param args Command line arguments (not used)
+     */ 
+    public static void main(String[] args)
+    {
+        // Create a new instance of the application and make the currently
+        // running thread the application's event dispatch thread.
+        MyApp theApp = new MyApp();       
+        theApp.enterEventDispatcher();
+    }
+    
+
+    /**
+     * Creates a new MyApp object
+     */
+    public MyApp()
+    {        
+        // Push a screen onto the UI stack for rendering.
+        pushScreen(new MyScreen());
+    }    
+}
